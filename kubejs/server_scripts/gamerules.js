99a64acd8d694cc3be4f53757545d38f72cf2a34
Youn Mélois <youn@melois.dev>
2026-04-26 02:28:27 +0200

    use kubejs to apply gamerules
    Signed-off-by: Youn Mélois <youn@melois.dev>

---
+++ kubejs/server_scripts/gamerules.js
@@ -0,0 +1,9 @@
+const GameRules = Java.loadClass("net.minecraft.world.level.GameRules");
+
+ServerEvents.loaded((event) => {
+    const server = event.server;
+    const rules = server.overworld().getGameRules();
+
+    rules.getRule(GameRules.RULE_DOINSOMNIA).set(false, server);
+    rules.getRule(GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE).set(0, server);
+});
