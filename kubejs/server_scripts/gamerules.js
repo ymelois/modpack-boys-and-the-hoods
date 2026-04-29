47b8b6843f62b86e8c9c6fa488a42a91fab7355c
Youn Mélois <youn@melois.dev>
2026-04-29 20:20:18 +0200

    disable fire spreading
    Signed-off-by: Youn Mélois <youn@melois.dev>

--- kubejs/server_scripts/gamerules.js
+++ kubejs/server_scripts/gamerules.js
@@ -4,6 +4,7 @@ ServerEvents.loaded((event) => {
     const server = event.server;
     const rules = server.overworld().getGameRules();
 
+    rules.getRule(GameRules.RULE_DOFIRETICK).set(false, server);
     rules.getRule(GameRules.RULE_DOINSOMNIA).set(false, server);
     rules.getRule(GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE).set(0, server);
 });
